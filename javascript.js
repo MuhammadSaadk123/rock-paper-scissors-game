1450f64be544880d6a00f9fb0a404bb2f6e904b3
MuhammadSaadk123 <muhammadsaadk123456@gmail.com>
2023-07-23 22:02:26 -0400

    still-need-to-loop-rounds-until-someone-wins-5

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 let computerSelection;
-function n() {
+let outcome;
+let computerDecision
+//Create a function that randomizes a choice for the computer
+function ComputerDecision() {
 let getComputerChoice=Math.floor(Math.random() * 3)
     if (getComputerChoice===0) {
         computerSelection="rock"
@@ -8,11 +11,39 @@ let getComputerChoice=Math.floor(Math.random() * 3)
         computerSelection="paper"
     }
     else {computerSelection="scissors"}
-console.log(computerSelection)
+return computerSelection
 }
-n()
-
-//Create a function that randomizes a choice for the computer
+console.log(ComputerDecision())
 //Create a variable that prompts a choice from the user
+let userChoice=prompt("Make a choice")
+
 //Create a function that simulates a round between the computer and the user
+function playRound(ComputerDecision, userChoice) {
+    if (ComputerDecision==userChoice) {
+       outcome="draw"
+    }
+     else if (userChoice=="rock" && ComputerDecision=="scissors") {
+        outcome="Congratulations! Rock beats scissor"
+     }
+     else if (userChoice=="rock" && ComputerDecision=="paper") {
+         outcome="Commiserations! Paper beats rock"
+     }
+     else if (userChoice=="paper" && ComputerDecision=="scissors") {
+         outcome="Commiserations! Scissor beats paper"
+     }
+     else if (userChoice=="paper" && ComputerDecision=="rock") {
+        outcome="Congratulations! Paper beats rock"
+     }
+     else if (userChoice=="scissors" && ComputerDecision=="rock") 
+     {outcome="Commiserations! Rock beats scissors"} 
+
+     else if (userChoice=="scissors" && ComputerDecision=="paper") 
+        {outcome="Congratulations! Scissors beats paper"}
+    else if(userChoice!="rock" || userChoice!="paper" ||userChoice!="scissors") {
+        outcome="Please choose either rock, paper, or scissors"
+    }
+ alert(outcome)
+ }
+ playRound(computerSelection, userChoice)
+
 //Loop that function until either the computer or the user has one five times
