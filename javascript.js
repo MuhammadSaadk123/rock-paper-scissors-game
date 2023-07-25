2e22bebab62a6feeab38f7ed42e61a57af47ee1f
MuhammadSaadk123 <muhammadsaadk123456@gmail.com>
2023-07-25 13:05:08 -0400

    basically finished

--- javascript.js
+++ javascript.js
@@ -15,49 +15,56 @@ let getComputerChoice=Math.floor(Math.random() * 3)
     else {computerSelection="scissors"}
 return computerSelection
 }
-console.log(ComputerDecision())
-//Create a variable that prompts a choice from the user
-let userChoiceOne;
-userChoiceOne=prompt("Make a choice")
-let userChoice=userChoiceOne.toLowerCase()
-for (; userChoice!=="rock" && userChoice!=="paper" && userChoice!="scissors"; ) {
-    {userChoiceOne=prompt("Choose either rock, paper, or scissors")
-    userChoice=userChoiceOne.toLowerCase()}
-}
-alert(userChoice)
+
 //Create a function that simulates a round between the computer and the user
 function playRound(ComputerDecision, userChoice) {
     if (ComputerDecision==userChoice) {
        outcome="Draw."
     }
      else if (userChoice=="rock" && ComputerDecision=="scissors") {
-        outcome="Congratulations! Rock beats scissor";
+        outcome="Congratulations! Rock beats scissors.";
         playerScore++;
      }
      else if (userChoice=="rock" && ComputerDecision=="paper") {
-         outcome="Commiserations! Paper beats rock";
+         outcome="Commiserations! Paper beats rock.";
          computerScore++;
      }
      else if (userChoice=="paper" && ComputerDecision=="scissors") {
-         outcome="Commiserations! Scissor beats paper";
+         outcome="Commiserations! Scissor beats paper.";
          computerScore++;
      }
      else if (userChoice=="paper" && ComputerDecision=="rock") {
-        outcome="Congratulations! Paper beats rock";
+        outcome="Congratulations! Paper beats rock.";
         playerScore++;
      }
      else if (userChoice=="scissors" && ComputerDecision=="rock") 
-     {outcome="Commiserations! Rock beats scissors";
+     {outcome="Commiserations! Rock beats scissors.";
       computerScore++;} 
 
      else if (userChoice=="scissors" && ComputerDecision=="paper") 
-        {outcome="Congratulations! Scissors beats paper"
+        {outcome="Congratulations! Scissors beats paper."
         playerScore++;}
     else if(userChoice!="rock" || userChoice!="paper" ||userChoice!="scissors") {
-        outcome="Please choose either rock, paper, or scissors"
+        outcome="Please choose either rock, paper, or scissors."
     }
  alert(outcome + " " + "The current score is" + " " + playerScore + "-" + computerScore)
  }
- playRound(computerSelection, userChoice)
 
-//Loop that function until either the computer or the user has one five times
+ //Loop a game between the computer and the user until either the computer or the user has one five times
+ for (; playerScore<5 && computerScore<5;) {
+ComputerDecision()
+console.log(computerSelection)
+userChoiceOne=prompt("Make a choice")
+userChoice=userChoiceOne.toLowerCase()
+for (; userChoice!=="rock" && userChoice!=="paper" && userChoice!="scissors"; ) {
+    {userChoiceOne=prompt("Please choose either rock, paper, or scissors.")
+    userChoice=userChoiceOne.toLowerCase()}
+}
+playRound(computerSelection, userChoice)}
+
+if (playerScore=5 && computerScore!==5) {
+    alert("Congratulations! You have won the game.")
+}
+else if (computerScore=5 && playerScore!==5) {
+    alert("Commiserations! You have lost the game.")
+}
